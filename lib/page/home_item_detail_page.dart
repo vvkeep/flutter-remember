738da47e38ae75491e3cf568238304c0c22af993
devyao <15919688564@163.com>
2021-09-07 09:43:28 +0800

    add item detail page

---
+++ lib/page/home_item_detail_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:remember/config/style.dart';
+import 'package:remember/mock/mock.dart';
+import 'package:remember/page/widget/home_category_item_widget.dart';
+import 'package:remember/page/widget/home_search_bar_widget.dart';
+import 'package:remember/router/routers.dart';
+import 'package:get/get.dart';
+
+class HomeItemDetailPage extends StatefulWidget {
+  HomeItemDetailPage({Key? key}) : super(key: key);
+
+  @override
+  _HomeItemDetailPageState createState() => _HomeItemDetailPageState();
+}
+
+class _HomeItemDetailPageState extends State<HomeItemDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: null,
+    );
+  }
+}
