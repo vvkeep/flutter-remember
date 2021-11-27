3bb6db49d414ad2f1bc98f3c233435dc251b586c
姚巍 <15919688564@163.com>
2021-11-28 01:12:38 +0800

    增加文件存储工具类

---
+++ lib/utils/storage_utils.dart
@@ -0,0 +1,32 @@
+import 'dart:ffi';
+import 'dart:io';
+import 'dart:ui';
+
+import 'package:path_provider/path_provider.dart';
+import 'package:get/get.dart';
+
+class StorageUtils {
+  Future<String> get _localPath async {
+    final _path = await getTemporaryDirectory();
+    return _path.path;
+  }
+
+  Future<File> localFile(String name) async {
+    final path = await _localPath;
+    return File('$path/cname');
+  }
+
+  Future<bool> saveFile(String name, Object val) async {
+    try {
+      final file = await localFile(name);
+      IOSink sink = file.openWrite();
+      sink.write(val);
+      sink.close();
+      return true;
+    } catch (e) {
+      //写入错误
+      printError(info: "文件写入错误: $e");
+      return false;
+    }
+  }
+}
